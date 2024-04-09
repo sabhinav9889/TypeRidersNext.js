e1c17694564e63c82c2baa136a218e334554dbab
Abhinav Singh <112590979+sabhinav9889@users.noreply.github.com>
2024-04-10 02:40:48 +0530

    up10

--- app/components/graph.tsx
+++ app/components/graph.tsx
@@ -1,5 +1,5 @@
 "use client"
-import { useEffect, useRef, useState } from "react";
+import { useEffect, useState } from "react";
 import { Chart } from "chart.js";
 function Example({wpmList, graph}:any) {
   // const [wpmList, setWpmList] = useState<number[]>([]);
@@ -9,31 +9,29 @@ function Example({wpmList, graph}:any) {
   for(let i = 0; i <wpmList.length; i++){
     lis[i] = i+1;
   }
-  const myChartDiv = useRef(null);
   useEffect(() => {
-    if(myChartDiv&&myChartDiv.current){
-      if(!myChartDiv.current.getContext('2d')) return;
-      const ctx = myChartDiv.current.getContext('2d');
-      const myChart = new Chart(ctx, {
-        type: 'line',
-        data: {
-          labels: graph,
-          datasets: [{
-            data: wpmList,
-            label: "WPM",
-            borderColor: "#3e95cd",
-            backgroundColor: "#7bb6dd",
-            fill: false,
-          },
-          {
-            data: temp,
-            label: "Time",
-            fill: false,
-          }
-          ]
+    const ele = document.getElementById('myChart') as HTMLCanvasElement;
+    var ctx = ele.getContext('2d');
+    if(!ctx) return;
+    var myChart = new Chart(ctx, {
+      type: 'line',
+      data: {
+        labels: graph,
+        datasets: [{
+          data: wpmList,
+          label: "WPM",
+          borderColor: "#3e95cd",
+          backgroundColor: "#7bb6dd",
+          fill: false,
         },
-      });
-   }
+        {
+          data: temp,
+          label: "Time",
+          fill: false,
+        }
+        ]
+      },
+    });
   }, [wpmList]);
   return (
     <div className="w-1/2 h-80 absolute top-16 left-56 ml-44">
@@ -41,7 +39,7 @@ function Example({wpmList, graph}:any) {
       {/* <h1 className="w-[110px] mx-auto mt-10 text-xl font-semibold capitalize ">line Chart</h1>
       <div className="w-[1100px] h-screen flex mx-auto my-auto">
         <div className='border border-gray-400 pt-0 rounded-xl  w-full h-fit my-auto  shadow-xl'> */}
-          <canvas ref={myChartDiv}></canvas>
+          <canvas id='myChart'></canvas>
         {/* </div>
       </div> */}
     </div>
