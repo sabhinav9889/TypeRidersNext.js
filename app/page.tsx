25e9e7ec5a7ae35ce1f2f471e7fced625bfa1575
Abhinav Singh <112590979+sabhinav9889@users.noreply.github.com>
2024-08-04 22:37:36 +0530

    hideSelect

--- app/page.tsx
+++ app/page.tsx
@@ -16,8 +16,12 @@ export default function Home() {
   const themeSet = useRef<HTMLDivElement>(null);
   const contextValue = useContext(messageData);
   const {theme, setTheme, countDown, setCountDown, diff, setDiff, begin, setBegin, start, setStart, cur, setCur} = contextValue!;
+  const handleContextMenu = (event: Event) => {
+    event.preventDefault();
+  };
   if(themeSet.current){
     themeSet.current.addEventListener('mousemove',()=>setCur(true));
+    themeSet.current.addEventListener('contextmenu', handleContextMenu);
     if(theme===0){ themeSet.current.classList.remove("bg-gray-900"); themeSet.current.classList.remove("bg-orange-900"); themeSet.current.classList.remove("bg-slate-900"); themeSet.current.classList.remove("bg-pink-200"); themeSet.current.classList.remove("bg-pink-900"); themeSet.current.classList.add("bg-neutral-950"); themeSet.current.classList.remove("text-black"); themeSet.current.classList.add("text-gray-100"); }
     else if(theme===1){ themeSet.current.classList.remove("bg-neutral-950"); themeSet.current.classList.remove("bg-orange-900"); themeSet.current.classList.remove("bg-slate-900"); themeSet.current.classList.remove("bg-pink-900"); themeSet.current.classList.remove("bg-pink-200"); themeSet.current.classList.add("bg-gray-900"); themeSet.current.classList.remove("text-black"); themeSet.current.classList.add("text-gray-100");}
     else if(theme===2){ themeSet.current.classList.remove("bg-gray-900"); themeSet.current.classList.remove("bg-neutral-950"); themeSet.current.classList.remove("bg-pink-900"); themeSet.current.classList.remove("bg-slate-900"); themeSet.current.classList.remove("bg-pink-200"); themeSet.current.classList.add("bg-orange-900"); themeSet.current.classList.remove("text-black"); themeSet.current.classList.add("text-gray-100"); }
@@ -31,13 +35,13 @@ export default function Home() {
           <link rel="icon" href="/favicon.ico" />
         </Head>
         {(! start)&&
-        <div className="w-full flex justify-center">
+        <div className="w-full flex justify-center select-none ">
           <div className="mt-40">
            <p className="text-4xl h-20 text-orange-800 font-bold font-mono">TypeRiders</p>
            <button className="border hover:border-orange-800 mt-36 w-44 h-10 rounded-md hover:bg-orange-800" onClick={()=>setStart(true)}>Start</button>
           </div>
         </div>}
-        {(start)&&<div className={`${(cur)?'':'cursor-none'}`}>
+        {(start)&&<div className={`${(cur)?'':'cursor-none'} select-none`}>
         {(!begin)&&<UpNav/>}
         <SinglePlayer/>
         {(!begin)&&<DownNav/>}
