c18ced623cf6b2ca6c661cd94461e491145dbf13
Abhinav Singh <112590979+sabhinav9889@users.noreply.github.com>
2024-10-02 00:06:34 +0530

    slider-fix

--- app/home/slider.tsx
+++ app/home/slider.tsx
@@ -1,3 +1,4 @@
+"use client"
 import { useContext, useRef, useState} from "react";
 import {messageData} from "../context";
 import { myTheme } from "../constant";
@@ -8,7 +9,7 @@ const Slider = ()=>{
     const contextValue = useContext(messageData);
     const {theme, setTheme, countDown, setCountDown, diff, setDiff, begin, setBegin, start, setStart, cur, setCur, drawer, setDrawer, livewpm, setlivewpm, sound ,setSound, gar, setGarage} = contextValue!;
     return(
-        <>
+        <div>
           <div className={`overflow-hidden md:w-60 w-20 absolute z-30 right-0 min-h-screen`}>
           <div className={`w-72 min-h-screen absolute z-30 ${myTheme[theme]} border-orange-900 border rounded-sm transition-transform ${drawer?'translate-x-0' : 'translate-x-full'} duration-300 ease-in-out`}>
             <div className="w-full mt-16 md:flex md:justify-centre">
@@ -22,11 +23,11 @@ const Slider = ()=>{
               </div>
               <p className="md:pl-4 pr-4 md:text-lg text-orange-200">Sound </p>
             </div>
-            <div className={`w-full mt-16 ml-2 md:flex justify-centre ${gar?'':'text-orange-200'}`}><FontAwesomeIcon className="mt-2" size="xl" icon={Icons[0]}/><p className={`md:ml-2  cursor-pointer text-lg  md:p-2`} onClick={()=>setGarage((prev)=>(prev)?false:true)}>Garage</p></div>
+            <div className={`w-full mt-16 ml-2 md:flex cursor-pointer  justify-centre ${gar?'':'text-orange-200'}`} onClick={()=>setGarage((prev)=>(prev)?false:true)}><FontAwesomeIcon className="mt-2" size="xl" icon={Icons[0]}/><p className={`md:ml-2 text-lg  md:p-2`}>Garage</p></div>
           </div>
           </div>
          <div onClick={()=>setDrawer(false)}></div>
-        </>
+        </div>
     )
 }
 
